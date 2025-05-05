eb4a92dcc869391718bffb8876d074bc24d94a0e
João Paulo Bezerra da Silva <jpsilva@stik.local>
2025-05-05 15:05:44 -0300

    api 1.2

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from server import create_connection
 from insert import insert_data
+import os
 
 app = Flask(__name__)
 CORS(app)
