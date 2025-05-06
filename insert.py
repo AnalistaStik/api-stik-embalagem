462d176ae4eb5c028b97a69c1875766b0dff9cf5
João Paulo Bezerra da Silva <jpsilva@stik.local>
2025-05-06 15:14:12 -0300

    api 2.0

--- insert.py
+++ insert.py
@@ -1,40 +1,11 @@
-def insert_data(conn, data):
-    try:
-        print(f"📥 Dados recebidos: {data}")
-        cursor = conn.cursor()
-
-        query = """
-        INSERT INTO registros (data, ordem_producao, quantidade, artigo, cor, peso, conferente, turno)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-        """
-
-        values = (
-            data['data'],
-            data['ordem_producao'],
-            data['quantidade'],
-            data['artigo'],
-            data['cor'],
-            data['peso'],
-            data['conferente'],
-            data['turno']
-        )
-
-        cursor.execute(query, values)
-        conn.commit()
-        print("✅ Inserção feita com sucesso.")
-        return True
-    except Exception as e:
-        print(f"❌ Erro ao inserir no banco: {e}")
-        return False
-
 # def insert_data(conn, data):
 #     try:
 #         print(f"📥 Dados recebidos: {data}")
 #         cursor = conn.cursor()
 
 #         query = """
-#         INSERT INTO Registros (data, ordem_producao, quantidade, artigo, cor, peso, conferente, turno)
-#         VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+#         INSERT INTO registros (data, ordem_producao, quantidade, artigo, cor, peso, conferente, turno)
+#         VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 #         """
 
 #         values = (
@@ -52,13 +23,41 @@ def insert_data(conn, data):
 #         conn.commit()
 #         print("✅ Inserção feita com sucesso.")
 #         return True
-
 #     except Exception as e:
-#         import traceback
-#         print("❌ ERRO COMPLETO NO INSERT:")
-#         traceback.print_exc()  # 👈 Mostra toda a stack de erro
 #         print(f"❌ Erro ao inserir no banco: {e}")
 #         return False
+
+from datetime import datetime
+
+def insert_data(conn, data):
+    try:
+        print(f"📥 Dados recebidos: {data}")
+        cursor = conn.cursor()
+
+        query = """
+        INSERT INTO registros (data, ordem_producao, quantidade, artigo, cor, peso, conferente, turno)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        """
+
+        values = (
+            datetime.strptime(data['data'], "%Y-%m-%d").date(),  # <- FORÇA tipo DATE
+            data['ordem_producao'],
+            data['quantidade'],
+            data['artigo'],
+            data['cor'],
+            data['peso'],
+            data['conferente'],
+            data['turno']
+        )
+
+        cursor.execute(query, values)
+        conn.commit()
+        print("✅ Inserção feita com sucesso.")
+        return True
+    except Exception as e:
+        print(f"❌ Erro ao inserir no banco: {e}")
+        return False
+
 
 
 
